3aaae181c965857d8591829a318572e072195823
Kowalski Prime <Kowalski Prime>
2016-07-29 14:41:58 +0200

    Aggiunto kaffe e età

--- Perzona.java
+++ Perzona.java
@@ -1,10 +1,12 @@
 public class Perzona implements Comparable<Kane> {
 	boolean perzona;
-	public Perzona(boolean perzona) {
+	int età;
+	public Perzona(boolean perzona, int età) {
 		this.perzona = perzona;
+		this.età = età;
 	}
 	public static Perzona getPerzonaFalza() {
-		return new Perzona(false);
+		return new Perzona(false,40);
 	}
 	public void REDIMI() {
 	    /*la perzona falsa kiede skusa e diventa kome i kani, solo un pò  peggio*/
@@ -15,10 +17,18 @@ public class Perzona implements Comparable<Kane> {
 			return "SN MEJO I KANID DELE PERZONE FALSE1!1!!";
 		return "I KANI SONO MEJO DELE PERZONE11!!1";
 	}
+	public int getEtà() {
+		return età;
+	}
 	@Override
 	public int compareTo(Kane o) {
 		if(perzona=false)
 			return -100000;
 		return -10000;
+	}
+	public String Kaffè() {
+		if(this.getEtà()>=40)
+			return "Buongiornissimooo kaffèèèèè?!!? 1!";
+		return null;
 	}
 }
