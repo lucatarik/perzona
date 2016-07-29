6774b81e942ea2b5e6a18f904f29e5e2cc9e3217
mlx <mlx>
2016-07-29 21:13:50 +0200

    [agiunto] agiunta kla$e renzie

---
+++ Renzie.java
@@ -0,0 +1,25 @@
+public class Renzie extends Perzona {
+  public long mortiSuaaaaaaaaa;
+
+  public Renzie(long mortiSuaaaaaaaaa) {
+		  super(false /*fa parteggiando capo della ka$ta11!!!*/, 69);
+		  this.mortiSuaaaaaaaaa = mortiSuaaaaaaaaa;
+	}
+
+  public Perzona regala80Euro(Perzona povera) {
+    povera.euri +=80;
+    return riprenditi80Euro(povera);
+  }
+  
+  public Perzona riprenditi80Euro(Perzona piuPovera)
+  {
+      piuPovera.euri -=80;
+      piuPovera.euri -=80;
+      piuPovera.euri -=80;
+      piuPovera.euri -=80;
+      piuPovera.euri -=80;
+      return piuPovera;
+  }
+  
+  
+}
